cbf792ecf3689f8bce60e01ca189e60d197252cd
Aarti Meena <aartimeena8405@gmail.com>
2024-09-19 13:25:00 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,11 @@
+ // Floating buttons slight movement based on scroll
+window.addEventListener('scroll', function() {
+    const donateButton = document.querySelector('.floating-donate');
+    const volunteerButton = document.querySelector('.floating-volunteer');
+    
+    let scrollPos = window.scrollY;
+    
+    donateButton.style.transform = `translateY(${scrollPos * 0.2}px)`;
+    volunteerButton.style.transform = `translateY(${scrollPos * 0.2}px)`;
+  });
+  
